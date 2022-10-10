4528154ce77b35e43073882432f05d1ab70a0fd8
mozoezomoe <dres2112@gmail.com>
2022-10-10 16:10:40 +0200

    error bars on report

--- scripts/ApparentFirstOrderEa_Die.R
+++ scripts/ApparentFirstOrderEa_Die.R
@@ -1,3 +1,4 @@
+#create factors for covariate modelling
 # libraries
 library(tidyverse)
 library(ggplot2)
@@ -5,6 +6,7 @@ library(texreg)
 library(nlme)
 source("scripts/r2adj.R")
 library(tidyr)
+library(nlraa)
 
 # read the data
 presiones1.1 <- read.csv("data/presiones1.csv", sep = ";", dec = ",")
@@ -15,7 +17,6 @@ presiones.l$Temp<-factor(substr(presiones.l$X,0,3))
 levels(presiones.l$Temp)<-c(4,20,4,20,4,20)
 presiones.l$Temp<-as.numeric(as.character(presiones.l$Temp))
 
-#create factors for covariate modelling
 presiones.l$sweetener<-factor(presiones.l$sweetener)
 presiones.l$processing<-factor(presiones.l$processing)
 presiones.l$rep<-factor(presiones.l$rep)
@@ -69,9 +70,13 @@ ant.simul2<-expand.grid(tiempo=seq(0,90,length=30),
 ant.simul2$sweetener<-factor(with(ant.simul2,substr(grouping,0,2)))
 ant.simul2$processing<-factor(with(ant.simul2,substr(grouping,4,4)))
 ant.simul2$grouping<-with(ant.simul2,sweetener:processing:factor(Temp))
-
-
 ant.simul2$prediction<-predict(ant.fm3,newdata = ant.simul2)
+
+# añadimos confidence bands
+
+fm3.dm <- predict_gnls(ant.fm3, newdata = ant.simul2, interval = "conf")
+ant.simul2.dm <- cbind(ant.simul2, fm3.dm)
+
 
 ggplot(ant.simul2, 
        aes(x = tiempo, y = prediction, col = factor(Temp))) +
@@ -80,55 +85,86 @@ ggplot(ant.simul2,
   xlab("Storage Time [Days]")+ylab("Concentration [mg/100mL]")
 # ggsave(filename="Figure1.pdf")
 
-library(nlraa)
 
-set.seed(123)
-
-ant_sim_unc <- simulate_gnls(ant.fm3, newdata = ant.simul2, nsim = 100, psim = 1, level = 1, value = "data.frame" )
 
 
 # ST:1
 
-ggplot(data = subset(ant.simul2,grouping == "ST:1:20"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
+ggplot(data = subset(ant.simul2.dm,grouping == "ST:1:20"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
   facet_wrap(compound~.,scales="free")+
-  geom_point()+ geom_point(data=subset(anthocyanins,grouping == "ST:1:4"), aes(x=tiempo, y=concentration, col = factor(Temp)))+
-  geom_line(data = ant.simul2 %>% filter(grouping == "ST:1:20"), aes(x=tiempo, y = sim))+
+  geom_point()+
+  geom_point(data=subset(anthocyanins,grouping == "ST:1:4"), aes(x=tiempo, y=concentration, col = factor(Temp)))+
+  geom_errorbar(aes(x = tiempo, ymin= Q2.5, ymax = Q97.5), alpha=0.2)+
   ggtitle("ST:1 20º modelled | 4º experimental") + xlab("Storage Time [Days]")+ylab("Concentration [mg/100mL]")
 
 # ST:2
-ggplot(data = ant.simul2 %>% filter(grouping == "ST:2:4"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
+ggplot(data = ant.simul2.dm %>% filter(grouping == "ST:2:4"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
   facet_wrap(compound~.,scales="free")+
-  geom_point()+geom_point(data=anthocyanins %>% filter(grouping == "ST:2:20"), aes(x=tiempo, y=concentration, col = factor(Temp)))+
+  geom_point()+
+  geom_point(data=anthocyanins %>% filter(grouping == "ST:2:20"), aes(x=tiempo, y=concentration, col = factor(Temp)))+
+  geom_errorbar(aes(x = tiempo, ymin= Q2.5, ymax = Q97.5), alpha=0.2)+
   ggtitle("SU:1 20º experimental | 4º modelled")+ xlab("Storage Time [Days]")+ylab("Concentration [mg/100mL]")
 
 # ST:P
-ggplot(data = ant.simul2 %>% filter(grouping == "ST:P:20"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
+ggplot(data = ant.simul2.dm %>% filter(grouping == "ST:P:20"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
   facet_wrap(compound~.,scales="free")+
   geom_point()+geom_point(data=anthocyanins %>% filter(grouping == "ST:P:4"), aes(x=tiempo, y=concentration, col = factor(Temp)))+
+  geom_errorbar(aes(x = tiempo, ymin= Q2.5, ymax = Q97.5), alpha=0.2)+
   ggtitle("ST:P 20º modelled | 4º experimental")+ xlab("Storage Time [Days]")+ylab("Concentration [mg/100mL]")
 
 # SU:1
 
-ggplot(data = ant.simul2 %>% filter(grouping == "SU:1:4"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
+ggplot(data = ant.simul2.dm %>% filter(grouping == "SU:1:4"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
   facet_wrap(compound~.,scales="free")+
-  geom_point()+geom_point(data=anthocyanins %>% filter(grouping == "SU:1:20"), aes(x=tiempo, y=concentration, col = factor(Temp))) +
+  geom_point()+
+  geom_point(data=anthocyanins %>% filter(grouping == "SU:1:20"), aes(x=tiempo, y=concentration, col = factor(Temp))) +
+  geom_errorbar(aes(x = tiempo, ymin= Q2.5, ymax = Q97.5), alpha=0.2)+
   ggtitle("SU:1 20º experimental | 4º modelled")+ xlab("Storage Time [Days]")+ylab("Concentration [mg/100mL]")
 
 # SU:2
 
-ggplot(data = ant.simul2 %>% filter(grouping == "SU:2:20"), aes(x=tiempo, y =prediction, col = factor(Temp)))+
+ggplot(data = ant.simul2.dm %>% filter(grouping == "SU:2:20"), aes(x=tiempo, y =prediction, col = factor(Temp)))+
   facet_wrap(compound~.,scales="free")+
-  geom_point()+geom_point(data=anthocyanins %>% filter(grouping == "SU:2:4"), aes(x=tiempo, y=concentration, col = factor(Temp))) +
+  geom_point()+
+  geom_point(data=anthocyanins %>% filter(grouping == "SU:2:4"), aes(x=tiempo, y=concentration, col = factor(Temp))) +
+  geom_errorbar(aes(x = tiempo, ymin= Q2.5, ymax = Q97.5), alpha=0.2)+
   ggtitle("SU:2 20º modelled | 4º experimental")+ xlab("Storage Time [Days]")+ylab("Concentration [mg/100mL]")
 
 # SU:P
 
-ggplot(data = ant.simul2 %>% filter(grouping == "SU:P:4"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
+ggplot(data = ant.simul2.dm %>% filter(grouping == "SU:P:4"), aes(x=tiempo, y = prediction, col = factor(Temp)))+
   facet_wrap(compound~.,scales="free")+
-  geom_point()+geom_point(data=anthocyanins %>% filter(grouping == "SU:P:20"), aes(x=tiempo, y=concentration, col = factor(Temp))) +
+  geom_point()+
+  geom_point(data=anthocyanins %>% filter(grouping == "SU:P:20"), aes(x=tiempo, y=concentration, col = factor(Temp))) +
+  geom_errorbar(aes(x = tiempo, ymin= Q2.5, ymax = Q97.5), alpha=0.2)+
   ggtitle("SU:P 20º experimental | 4º modelled")+ xlab("Storage Time [Days]")+ylab("Concentration [mg/100mL]")
 
 
 
+# pruebas
 
+
+data(Orange)
+
+fmoL <- nlsList(circumference ~ SSlogis(age, Asym, xmid, scal), data = Orange)
+
+fmo <- nlme(fmoL, random = pdDiag(Asym + xmid + scal ~ 1))
+
+sdat10 <- simulate_nlme(fmo, nsim = 100, psim = 1, level = 0, value = "data.frame")
+
+ggplot(data = sdat10) + 
+  geom_line(aes(x = age, y = sim.y, group = ii), color = "gray", alpha = 0.5) + 
+  geom_point(aes(x = age, y = circumference)) + 
+  ggtitle("psim = 1, level = 0") + 
+  ylab("circumference")
+
+set.seed(123)
+
+fmoL <- nlsList(prediction ~ compound+Temp, data = ant.simul2)
+
+
+ant_sim_unc <- simulate_gnls(ant.fm3, newdata = ant.simul2, nsim = 100, psim = 1, level = 1, value = "data.frame")
+ant.simul2$sim <- ant_sim_unc
+
+#
 
